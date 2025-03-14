083a4dd5ed23492473b9ff1bc73ff0edd18cd9ee
Jeanpiere Laura <jean.ganador12@gmail.com>
2025-03-14 01:47:55 -0500

    Initial Commit

---
+++ JBudget/JBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  JBudgetApp.swift
+//  JBudget
+//
+//  Created by Jeanpiere Laura on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct JBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JBudgetTests/JBudgetTests.swift
@@ -0,0 +1,16 @@
+//
+//  JBudgetTests.swift
+//  JBudgetTests
+//
+//  Created by Jeanpiere Laura on 14/03/25.
+//
+
+import Testing
+
+struct JBudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
